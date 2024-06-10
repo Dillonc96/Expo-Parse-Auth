18949a5cdc2da816017cf01656f78a3d8fdec3c1
Craig Dillon <equiconnect2023@gmail.com>
2024-06-10 22:22:03 +0100

    Update Password reset screen

--- app/(public)/restorePassword.tsx
+++ app/(public)/restorePassword.tsx
@@ -1,11 +1,55 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Text, View, TextInput } from '@/components/Themed';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { Button } from 'react-native';
+import { Alert, Button } from 'react-native';
 import Styles from '@/components/UniversalStyles';
-
+import { router } from 'expo-router';
+import handleServerError from '@/functions/ServerErrorAlert';
 
 export default function restorePassword() {
+
+    // Your state variable
+  const [email, setEmail] = useState("");
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+  const doUserPasswordReset = async function () {
+    // Reset password
+    const resetPassword = async ({ email }: { email: string }) => {
+      try {
+        const result = await Parse.Cloud.run("resetPassword", { email });
+        //check email format
+        if (!emailRegex.test(email)) {
+          Alert.alert("Invalid email", "Please enter a valid email address");
+          console.log("not an email");
+          return;
+        }
+        if (!result.success) {
+          Alert.alert("Failed to reset password", result.message);
+          return;
+        }
+        Alert.alert(
+          "Success!",
+          `Please check ${email} to proceed with password reset.`
+        );
+        // Redirect user to your login screen
+        router.push("/index");
+        console.log(result);
+
+        return result;
+      } catch (error: any) {
+        console.error(error);
+        handleServerError(
+          "Server Error: Can't curently reset password",
+          error
+        );
+      }
+    };
+    resetPassword({ email });
+
+    // Note that this value come from state variables linked to your text input
+  };
+
+
   return (
     <SafeAreaView style={{flex:1}}>
         <View style={{justifyContent: "center", flex:1, alignItems:"center"}}>
